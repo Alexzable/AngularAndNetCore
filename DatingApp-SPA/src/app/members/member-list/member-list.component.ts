1551cfbba9eec99666a1f8eb57c987988e3adda3
EBS\alexandru.boldisor <alexandru.boldisor@nttdata.ro>
2020-04-24 16:55:36 +0300

    End of section 14 Pagin Sorting Filtering -paging in the api/spa -helpers in the api created -ngx Boostrap for pagination -filtering

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -1,8 +1,10 @@
+import { Pagination, PaginatedResult } from './../../_models/pagination';
 import { AlertifyService } from '../../_services/alertify.service';
 import { UserService } from '../../_services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../_models/user';
 import { ActivatedRoute } from '@angular/router';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-member-list',
@@ -11,6 +13,11 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class MemberListComponent implements OnInit {
   users: User[];
+  user: User = JSON.parse(localStorage.getItem('user'));
+  genderList = [{value: 'male', display: 'Males'}, {value: 'female', display: 'Females'}];
+  userParams: any = {};
+  pagination: Pagination;
+
   constructor(
     private userSerivce: UserService,
     private alertify: AlertifyService,
@@ -19,7 +26,36 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit() {
     this.route.data.subscribe(data => {
-      this.users = data['users'];
+      this.users = data['users'].result;
+      this.pagination = data['users'].pagination;
+    });
+
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadUsers();
+  }
+
+  resetFilters() {
+    this.userParams.gender = this.user.gender === 'female' ? 'male' : 'female';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.userParams.orderBy = 'lastActive';
+    this.loadUsers();
+  }
+
+  loadUsers() {
+    this.userSerivce.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+    .subscribe((res: PaginatedResult<User[]>) => {
+      this.users = res.result;
+      this.pagination = res.pagination;
+    // tslint:disable-next-line: no-shadowed-variable
+    }, error => {
+      this.alertify.error(error);
     });
   }
 }
